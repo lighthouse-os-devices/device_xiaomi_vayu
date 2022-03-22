49cda36d9850895e01eb795d8d0e429326b04e94
CLHEX <clhexftw@protonmail.com>
2022-03-22 15:59:32 +0530

    vayu: Project Lighthouse Bringup
    Signed-off-by: CLHEX <clhexftw@protonmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vayu.mk
+    $(LOCAL_DIR)/lighthouse_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vayu-eng \
-    arrow_vayu-userdebug \
-    arrow_vayu-user
+    lighthouse_vayu-eng \
+    lighthouse_vayu-userdebug \
+    lighthouse_vayu-user
